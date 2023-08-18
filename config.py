972c4c57ab5534e8a0891e39aeb1275b4d14d485
rain0x1 <Jennymartinezxx17@gmail.com>
2023-08-18 18:05:17 +0300

    new changes

---
+++ config.py
@@ -0,0 +1,10 @@
+from urllib import parse
+
+TOKEN = 'MTE0MDc0MDE0NjA5OTEzODY0MA.Gz8dYA.Ux_bHbot8Dr9GZYHKkuTMQeX92QZYeRnERB7aA'
+CLIENT_SECRET = 'SnwhkeIUwg3AIKZ7aSpUUQGUbbQc8Qlo'
+REDIRECT_URL = 'http://localhost:5000/oauth/callback'
+REDIRECT_URL_LEAGUE = 'https://matches.arbiters.io/oauth/callback/league'
+REDIRECT_URL_COUNTER = 'https://matches.arbiters.io/oauth/callback/counter'
+OAUTH_URL = f'https://discord.com/api/oauth2/authorize?client_id=1140740146099138640&redirect_uri={parse.quote(REDIRECT_URL)}&response_type=code&scope=identify'
+OAUTH_URL_LEAGUE = f'https://discord.com/api/oauth2/authorize?client_id=1140740146099138640&redirect_uri={parse.quote(REDIRECT_URL_LEAGUE)}&response_type=code&scope=identify'
+OAUTH_URL_COUNTER = f'https://discord.com/api/oauth2/authorize?client_id=1140740146099138640&redirect_uri={parse.quote(REDIRECT_URL_COUNTER)}&response_type=code&scope=identify'
